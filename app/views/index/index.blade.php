adb85ba5c639f617fcc69b735da4d44ada7d6eee
imon1992 <andrey.kolotii@mail.ru>
2017-08-20 09:25:51 +0300

    first commit

---
+++ app/views/index/index.blade.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+
+
+<form method="POST" action="{{URL::to('/make-url')}}">
+Put URL: <input name="url" />
+<button type="submit"> Make URL </button>
+@if ($errors->first('url'))
+<p>{{{$errors->first('url')}}} </p>
+@endif
+</form>
+@if (Auth::check())
+<a href="{{URL::to('auth/logout')}}">Logout</a>
+@endif
+</body>
+
+</html>
